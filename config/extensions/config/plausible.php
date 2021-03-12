d586e019cac057c8bfe0d784ce676794493e0c3e
Hannes <hannes@octofox.de>
2021-03-12 21:48:50 +0100

    :wrench: Use WikiSEO master

--- config/extensions/config/plausible.php
+++ config/extensions/config/plausible.php
@@ -5,4 +5,11 @@ $wgPlausibleDomainKey = null;
 $wgPlausibleTrackOutboundLinks = false;
 $wgPlausibleTrackLoggedIn = false;
 $wgPlausibleEnableCustomEvents = true;
+$wgPlausibleHonorDNT = true;
 $wgPlausibleIgnoredTitles = [];
+
+$wgPlausibleTrack404 = true;
+$wgPlausibleTrackSearchInput = true;
+$wgPlausibleTrackEditButtonClicks = false;
+$wgPlausibleTrackCitizenSearchLinks = false;
+$wgPlausibleTrackCitizenMenuLinks = false;
