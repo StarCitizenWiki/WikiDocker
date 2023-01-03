d812e2677095fee0773b52b19205c867aa9d2750
Hannes <hannes@octofox.de>
2023-01-03 10:21:37 +0100

    refactor: Add 'webp' to allowed formats

--- config/extensions/config/uploadwizard.php
+++ config/extensions/config/uploadwizard.php
@@ -4,7 +4,7 @@ $wgCheckFileExtensions = true;
 $wgVerifyMimeType = true;
 $wgAllowCopyUploads = true;
 
-$wgFileExtensions = ['png', 'gif', 'jpg', 'jpeg', 'ppt', 'pptx', 'pdf', 'psd', 'mp3', 'xls', 'xlsx', 'swf', 'doc', 'docx', 'odt', 'odc', 'odp', 'odg', 'mpp', 'mp4', 'webm', 'zip', 'dae', 'svg', 'ctm', 'ogg', 'acc', 'opus', 'm4a'];
+$wgFileExtensions = ['png', 'gif', 'jpg', 'jpeg', 'ppt', 'pptx', 'pdf', 'psd', 'mp3', 'xls', 'xlsx', 'swf', 'doc', 'docx', 'odt', 'odc', 'odp', 'odg', 'mpp', 'mp4', 'webm', 'zip', 'dae', 'svg', 'ctm', 'ogg', 'acc', 'opus', 'm4a', 'webp'];
 
 $wgUploadWizardConfig = [
     'debug' => false,
