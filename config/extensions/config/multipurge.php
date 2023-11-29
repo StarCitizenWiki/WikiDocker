93a0499b5adbc7e1c2d4c0580ab604c5eaaa9c35
H. C. Kruse <hannes@octofox.de>
2023-11-29 18:30:34 +0100

    refactor: Remove varnish

--- config/extensions/config/multipurge.php
+++ config/extensions/config/multipurge.php
@@ -5,8 +5,8 @@ $wgMultiPurgeCloudFlareApiToken = getenv( 'EXT_MULTIPURGE_CF_TOKEN' );
 $wgMultiPurgeCloudFlareAccountId = getenv( 'EXT_MULTIPURGE_CF_ACCOUNT' );
 
 $wgMultiPurgeVarnishServers = [ 'http://star-citizen.wiki-varnish' ];
-$wgMultiPurgeEnabledServices = [ 'cloudflare', 'varnish' ];
-$wgMultiPurgeServiceOrder = [ 'varnish', 'cloudflare' ];
+$wgMultiPurgeEnabledServices = [ 'cloudflare' ];
+$wgMultiPurgeServiceOrder = [ 'cloudflare' ];
 
 $wgMultiPurgeStaticPurges = [
   'Load Script' => 'load.php?lang=de&modules=startup&only=scripts&raw=1&skin=citizen'
